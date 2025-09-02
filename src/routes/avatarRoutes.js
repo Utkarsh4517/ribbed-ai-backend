be57297de53ce482906a00e878eec60784d4c1bd
Utkarsh4517 <uttkarsh.dav@gmail.com>
2025-09-03 02:27:17 +0530

    added scene routes

--- src/routes/avatarRoutes.js
+++ src/routes/avatarRoutes.js
@@ -4,6 +4,8 @@ const avatarController = require('../controllers/avatarController');
 const router = express.Router();
 
 router.post('/create-avatar', avatarController.createAvatar);
+router.post('/create-scenes', avatarController.createScenes);
 router.get('/test-replicate', avatarController.testReplicate);
+router.get('/test-nano-banana', avatarController.testNanoBanana);
 
 module.exports = router;

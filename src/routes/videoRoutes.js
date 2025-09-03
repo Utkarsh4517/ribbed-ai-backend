b6e59051f969492a9ff2a16712a8921837df3098
Utkarsh4517 <uttkarsh.dav@gmail.com>
2025-09-03 22:43:58 +0530

    updated video routes

--- src/routes/videoRoutes.js
+++ src/routes/videoRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const videoController = require('../controllers/videoController');
 const videoService = require('../services/videoService');
+const { authMiddleware } = require('../middleware/authMiddleware');
 const router = express.Router();
+router.use(authMiddleware);
 router.post('/generate', videoController.submitVideoGeneration);
 router.get('/job/:jobId', videoController.getJobStatus);
 router.get('/user-jobs', videoController.getUserJobs);
+router.post('/job/:jobId/cancel', videoController.cancelJob);
+router.delete('/job/:jobId', videoController.deleteJob);
 router.post('/start-processor', async (req, res) => {
   try {
     const io = req.app.get('io');
